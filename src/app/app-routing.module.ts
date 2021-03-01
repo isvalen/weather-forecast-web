908dd4fcc8060f446a1b4d4b99f418bc886ef05d
isvalen <shoval.jamil@gmail.com>
2021-03-01 08:41:45 +0700

    Added route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,17 @@
-import { NgModule } from '@angular/core';
+import { CUSTOM_ELEMENTS_SCHEMA, NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { TodayComponent } from './today/today.component';
+import { SevenDaysComponent } from './sevendays/sevendays.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: TodayComponent },
+  { path: 'seven-days', component: SevenDaysComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA]
+
 })
 export class AppRoutingModule { }
